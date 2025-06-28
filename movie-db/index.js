5e1484bd6b87671340dd0ae019e59cd0840c7e78
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-06-28 17:25:27 +0530

    feat: added handlebars as a template engine

--- movie-db/index.js
+++ movie-db/index.js
@@ -1,14 +1,22 @@
 import morgan from "morgan";
 import express from "express";
-import { createWriteStream } from "fs";
-import { router as movieRouter } from "./movie/index.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { createWriteStream } from "fs";
+import { engine as expressHandlebars } from "express-handlebars";
+import { router as movieRouter } from "./movie/index.js";
 
 const app = express();
 const logStream = createWriteStream("access.log", { flags: "a" });
 
-app.set("view engine", "pug");
+// app.set("view engine", "pug");
+
+app.engine(
+  "handlebars",
+  expressHandlebars({ helpers: { uc: (data) => data.toUpperCase() } })
+);
+app.set("view engine", "handlebars");
+app.set("views", [`${dirname(fileURLToPath(import.meta.url))}/movie/views`]);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(`${dirname(fileURLToPath(import.meta.url))}/public`));
--- movie-db/movie/controller.js
+++ movie-db/movie/controller.js
@@ -1,11 +1,22 @@
 import { dirname } from "path";
+import { readFileSync } from "fs";
+import handlebars from "handlebars";
 import { fileURLToPath } from "url";
 import { getAll, remove } from "./model.js";
 
+const listItem = handlebars.compile(
+  readFileSync(
+    `${dirname(fileURLToPath(import.meta.url))}/views/list-item.handlebars`,
+    "utf-8"
+  )
+);
+
 export async function listAction(request, response) {
   const movies = await getAll();
-  response.render(`${dirname(fileURLToPath(import.meta.url))}/views/list`, {
+  response.render("list", {
+    layout: false,
     movies,
+    partials: { listItem },
   });
 }
 
