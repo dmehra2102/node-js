ce116e00e19620b0c5d4f8320e7b3248ad201d54
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-07-04 09:57:27 +0530

    feat: added more robust authentication system using passport library

--- app_authentication/index.js
+++ app_authentication/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import session from "express-session";
 import loginFormVars from "./form-data.js";
+import passport from "passport";
+import Account from "./models/Account.js";
 
 const PORT = 3000;
 const users = {};
@@ -10,6 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
 app.set("view engine", "hbs");
+app.use(
+  session({ secret: "cookie_secret", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(Account.serializeUser);
+passport.deserializeUser(Account.deserializeUser);
+passport.use("local", Account.genStrategy());
 
 router.get("/", (request, response) => {
   const { page } = request.query;
--- app_authentication/models/Account.js
+++ app_authentication/models/Account.js
@@ -1,4 +1,5 @@
 import db from "../dbConfig.js";
+import crypto, { hash } from "node:crypto";
 import { Model, STRING, TEXT } from "sequelize";
 import { Strategy as LocalStrategy } from "passport-local";
 
@@ -6,6 +7,71 @@ class Account extends Model {
   static async findByUsername(username) {
     const lowerCaseUsername = username.toLowerCase();
     return await this.findOne({ where: { username: lowerCaseUsername } });
+  }
+
+  static async register(username, password) {
+    const exisitingAccount = this.findByUsername(username);
+    if (exisitingAccount) {
+      throw new Error("Account already exists");
+    }
+    const account = this.build({ username });
+    await account.setPassword(password);
+    return await account.save();
+  }
+
+  setPassword(password) {
+    if (!passsword) throw new Error("No password supplied.");
+
+    return new Promise((resolce, reject) => {
+      try {
+        const bufferBytes = crypto.randomBytes(32);
+        const salt = bufferBytes.toString("hex");
+        const hashRaw = crypto.pbkdf2Sync(password, salt, 12000, 512, "sha1");
+        this.set("hash", Buffer.from(hashRaw).toString("hex"));
+        this.set("salt", salt);
+      } catch (error) {
+        reject(new Error("Unable to hash password"));
+      }
+    });
+  }
+
+  async authenticate(password, done) {
+    const { hash, salt } = this;
+    if (!salt) throw new Error("No salt found");
+
+    const hashRaw = crypto.pbkdf2Sync(password, salt, 12000, 512, "sha1");
+    const currentHash = Buffer.from(hashRaw).toString("hex");
+
+    if (currentHash === hash) {
+      return done(null, this);
+    } else {
+      done(null, false, { message: "Password Incorrect" });
+    }
+  }
+
+  static passwordAuthenticate() {
+    return async (username, password, done) => {
+      const account = await this.findByUsername(username);
+      if (account) {
+        return await account.authenticate(password, done);
+      } else {
+        return done(new Error("Unable to authenticate"));
+      }
+    };
+  }
+
+  static serializeUser(account, done) {
+    const { username } = account;
+    done(null, username);
+  }
+
+  static async deserializeUser(username, done) {
+    const foundAccount = await this.findByUsername(username);
+    done(null, foundAccount);
+  }
+
+  static genStrategy() {
+    return new LocalStrategy(this.passwordAuthenticate());
   }
 }
 
@@ -36,4 +102,4 @@ Account.beforeCreate((account) => {
 });
 
 Account.sync();
-module.exports = Account;
+export default Account;
