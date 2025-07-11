eecc59e8e395273000cf1e39fabd94d89abc92ab
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-07-11 20:58:55 +0530

    feat: added controller logic for getting employee data

--- mongoose/controllers/employee.controller.js
+++ mongoose/controllers/employee.controller.js
@@ -1,12 +1,19 @@
+import { EmployeeModel } from "../model";
+
 class EmployeeController {
   static findAll = async (req, res, next) => {
     try {
+      const employees = await EmployeeModel.find().lean().exec();
+      return res.status(200).json(employees);
     } catch (error) {
       next(error);
     }
   };
   static findById = async (req, res, next) => {
     try {
+      const { id } = req.params;
+      const employee = await EmployeeModel.findById(id);
+      return res.status(200).json(employee);
     } catch (error) {
       next(error);
     }
