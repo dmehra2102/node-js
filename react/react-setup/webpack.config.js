d9d10651c1e727707bbf6af9d2426d2c92c2e3ba
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-07-15 10:03:04 +0530

    feat: added example of manual react project setup from scratch

---
+++ react/react-setup/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
